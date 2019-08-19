2777fcb7e2f4988a760aa6da526ddd55c6b693d5
shalini-polapally <polapallyshalini1@gmail.com>
2019-08-19 16:44:06 +0530

    User authentication

--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
 import express from 'express';
 import path from 'path';
+import mongoose from 'mongoose';
+import bodyParser from 'body-parser';
+import auth from './routes/auth';
+import dotenv from 'dotenv';
+import Promise from 'bluebird';
 
-
+dotenv.config();
 const app = express();
+app.use(bodyParser.json());
+mongoose.Promise = Promise;
+mongoose.connect(process.env.MONGODB_URL, { useNewUrlParser: true });
 
-app.post('/api/auth', (req, res) => {
-    res.status(400).json({ errors: { global: "Invalid credentials" } })
-})
+app.use('/api/auth', auth)
 
 app.get('/*', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
